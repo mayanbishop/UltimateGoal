4f985a354a359cfab382229c63c4f9f81adbb5af
mayanbishop <nikhil.arayath@hmail.com>
2020-12-12 21:20:17 -0500

    Ring detection with webcam easy cv version

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RingWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RingWebcam.java
@@ -3,32 +3,33 @@ package org.firstinspires.ftc.teamcode.auto;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.opencv.core.Core;
+import org.firstinspires.ftc.teamcode.assembly.UltimateBot;
 import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Size;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvInternalCamera;
+
 import org.openftc.easyopencv.OpenCvPipeline;
 
 @Autonomous(name = "RingWebcam", group = "Qualifier")
 public class RingWebcam extends LinearOpMode
 {
     OpenCvCamera webCam;
-    SkystoneDeterminationPipeline pipeline;
+    RingDeterminationPipeline pipeline;
+    UltimateBot ultimateBot = new UltimateBot();
 
     @Override
     public void runOpMode()
     {
 
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        ultimateBot.initRobot(hardwareMap);
+
+        int cameraMonitorViewId = ultimateBot.getRobotHardware().getHwMap().appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webCam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "webcam"), cameraMonitorViewId);
-        pipeline = new SkystoneDeterminationPipeline();
+        pipeline = new RingDeterminationPipeline(telemetry);
         webCam.setPipeline(pipeline);
 
         // We set the viewport policy to optimized view so the preview doesn't appear 90 deg
@@ -49,116 +50,104 @@ public class RingWebcam extends LinearOpMode
 
         while (opModeIsActive())
         {
-            telemetry.addData("Analysis", pipeline.getAnalysis());
-            telemetry.addData("Position", pipeline.position);
+            telemetry.addData("numRings", pipeline.getNumRings());
             telemetry.update();
 
             // Don't burn CPU cycles busy-looping in this sample
-            sleep(50);
+            sleep(5);
         }
     }
 
-    public static class SkystoneDeterminationPipeline extends OpenCvPipeline
+    public static class RingDeterminationPipeline extends OpenCvPipeline
     {
-        /*
-         * An enum to define the skystone position
-         */
-        public enum RingPosition
-        {
-            FOUR,
-            ONE,
-            NONE
+
+        static Telemetry telemetry;
+
+        public RingDeterminationPipeline(Telemetry tele) {
+            telemetry = tele;
         }
 
-        /*
-         * Some color constants
-         */
-        static final Scalar BLUE = new Scalar(0, 0, 255);
-        static final Scalar GREEN = new Scalar(0, 255, 0);
-
-        /*
-         * The core values which define the location and size of the sample regions
-         */
-        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(181,98);
-
-        static final int REGION_WIDTH = 35;
-        static final int REGION_HEIGHT = 25;
-
-        final int FOUR_RING_THRESHOLD = 150;
-        final int ONE_RING_THRESHOLD = 135;
-
-        Point region1_pointA = new Point(
-                REGION1_TOPLEFT_ANCHOR_POINT.x,
-                REGION1_TOPLEFT_ANCHOR_POINT.y);
-        Point region1_pointB = new Point(
-                REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-
-        /*
-         * Working variables
-         */
-        Mat region1_Cb;
-        Mat YCrCb = new Mat();
-        Mat Cb = new Mat();
-        int avg1;
-
-        // Volatile since accessed by OpMode thread w/o synchronization
-        private volatile RingPosition position = RingPosition.FOUR;
-
-        /*
-         * This function takes the RGB frame, converts to YCrCb,
-         * and extracts the Cb channel to the 'Cb' variable
-         */
-        void inputToCb(Mat input)
-        {
-            Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
-            Core.extractChannel(YCrCb, Cb, 1);
-        }
+        int numRings;
 
         @Override
-        public void init(Mat firstFrame)
+        public  Mat processFrame(Mat input)
         {
-            inputToCb(firstFrame);
 
-            region1_Cb = Cb.submat(new Rect(region1_pointA, region1_pointB));
-        }
+            int startRow = 0;
+            int endRow = 0;
+            int startColumn = 0;
+            int endColumn = 0;
 
-        @Override
-        public Mat processFrame(Mat input)
-        {
-            inputToCb(input);
+            double[] pixel;
+            Size size = input.size();
+            double height = size.height;
+            double width = size.width;
 
-            avg1 = (int) Core.mean(region1_Cb).val[0];
+            double redValue = 0;
+            double greenValue = 0;
+            double blueValue = 0;
+            for (int i=100; i < width; i++)
+            {
+                for (int j=0; j < height; j++)
+                {
+                    pixel = input.get(i,j);
+                    if (pixel != null && pixel.length > 0)
+                    {
+                        redValue = pixel[0];
+                        greenValue = pixel[1];
+                        blueValue= pixel[2];
+                        if (redValue > 150 && greenValue > 50 && blueValue < 50) {
+                            if ( startRow <= 0)
+                                startRow = i;
+                            if (startColumn <= 0)
+                                startColumn = j;
 
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    region1_pointA, // First point which defines the rectangle
-                    region1_pointB, // Second point which defines the rectangle
-                    BLUE, // The color the rectangle is drawn in
-                    2); // Thickness of the rectangle lines
+                            if (endRow < i)
+                                endRow = i;
 
-            position = RingPosition.FOUR; // Record our analysis
-            if(avg1 > FOUR_RING_THRESHOLD){
-                position = RingPosition.FOUR;
-            }else if (avg1 > ONE_RING_THRESHOLD){
-                position = RingPosition.ONE;
-            }else{
-                position = RingPosition.NONE;
+                            if (endColumn < j)
+                                endColumn = j;
+                        }
+                    }
+                    else
+                    {
+                        //telemetry.addData("Pixel Null at",  i );
+                        //telemetry.addData(" ",  j );
+                        //telemetry.update();
+                    }
+                }
             }
 
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    region1_pointA, // First point which defines the rectangle
-                    region1_pointB, // Second point which defines the rectangle
-                    GREEN, // The color the rectangle is drawn in
-                    -1); // Negative thickness means solid fill
+            double ringWidth = endRow - startRow;
+            double ringHeight = endColumn - startColumn;
+
+            double ratio =  0;
+            if (ringHeight > 0.0)
+                ratio = ringWidth/ringHeight;
+
+
+            if (ratio > 0.7)
+                numRings = 4;
+            else if (ratio > 0.1 )
+                numRings = 1;
+            else
+                numRings = 0;
+
+            telemetry.addData("Start Row ",  startRow );
+            telemetry.addData("End Row ",  endRow ); //Display it on telemetry
+            telemetry.addData("Start Column ",  startColumn );
+            telemetry.addData("End Column ",  endColumn );
+            telemetry.addData("Ratio ",  ratio );
+            telemetry.addData("Number of Rings ",  numRings );
+            telemetry.update();
 
             return input;
         }
 
-        public int getAnalysis()
+
+        public int getNumRings()
         {
-            return avg1;
+            return numRings;
         }
     }
 }
